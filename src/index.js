d981ace17e73af9736c83b1774b00696dd00a55e
Miki22986 <elisa.miki@gmail.com>
2025-04-13 23:35:24 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+//serve a controllare che il documento sia interamente caricato
 document.addEventListener("DOMContentLoaded", () => {
   const todoList = document.querySelector(".todo-list");
   const emptyListMess = document.querySelector(".empty-list-message");
-  //serve a controllare che il documento sia interamente caricato
-  function prova(event) {
+  const text = document.querySelector("#text");
+
+  function addTask(event) {
     event.preventDefault();
-    alert("funziono");
+    const elementList = text.ariaValueMax.trim();
+    if (elementList) {
+      aggiungi(elementList);
+
+      elementList.value = ""; //se l'elemento è stato aggiunto svuota il campo
+    }
   }
+  function aggiungi() {}
 
   const form = document.querySelector("form");
-  form.addEventListener("submit", prova);
+  form.addEventListener("submit", addTask);
 });
