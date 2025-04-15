11f55427fcb57e8f22f19a663bac2fd84e26b093
Miki22986 <elisa.miki@gmail.com>
2025-04-15 22:27:21 +0200

    update javascript funzionante

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 //serve a controllare che il documento sia interamente caricato
 document.addEventListener("DOMContentLoaded", () => {
   const todoList = document.querySelector(".todo-list");
-  const emptyListMess = document.querySelector(".empty-list-message");
   const text = document.querySelector("#text");
   const button = document.querySelector("#additemli");
 
-  function addTask(event) {
-    event.preventDefault();
+  function addTask() {
     const elementList = text.value.trim();
     if (elementList) {
       aggiungi(elementList);
@@ -16,42 +14,59 @@ document.addEventListener("DOMContentLoaded", () => {
       alert("aggiungi un elemento");
     }
   }
-
   button.addEventListener("click", addTask);
 
   function aggiungi(elementList) {
     const ListItem = document.createElement("li"); //per creare elementi li
     ListItem.textContent = elementList; // Inserisce solo testo, ignora eventuali tag HTML.
     todoList.appendChild(ListItem); //attacca il li alla ul
-
+    emptyBox();
     CancellaTutto(ListItem);
     cancellaSingolo(ListItem);
   }
+
+  function emptyBox() {
+    const emptyListMess = document.querySelector(".empty-list-message");
+    if (todoList.children.length === 0) {
+      emptyListMess.textContent = "Non ci sono elementi";
+    } else {
+      emptyListMess.textContent = "";
+    }
+  }
+
   function CancellaTutto(ListItem) {
     const deleteButton = document.querySelector("#cancella");
     deleteButton.addEventListener("click", function () {
-      todoList.removeChild(ListItem);
+      todoList.innerHTML = "";
+      localStorage.removeItem("elementList");
+      saveElements();
+      emptyBox();
     });
   }
 
   function cancellaSingolo(ListItem) {
     ListItem.addEventListener("click", function () {
       this.remove();
+      localStorage.removeItem("elementList");
+      saveElements();
+      emptyBox();
     });
   }
   //la funzione sotto mette ciò che abbiamo inserito dentro il local store e lo rende visibile
   function saveElements() {
     //dichiara come variabile un array vuoto in cui inserire i li
-    const elementList = [];
+    const elementLists = [];
     todoList.querySelectorAll("li").forEach(function (item) {
-      elementList.push(item.textContent.trim());
+      elementLists.push(item.textContent.trim());
     });
-    localStorage.setItem("elementList", JSON.stringify(elementList));
+    localStorage.setItem("elementList", JSON.stringify(elementLists));
   }
   //lo riprende e lo mostra
   function loadItems() {
     const Items = JSON.parse(localStorage.getItem("elementList")) || [];
     Items.forEach(aggiungi);
+    emptyBox();
   }
   loadItems();
+  CancellaTutto();
 });
